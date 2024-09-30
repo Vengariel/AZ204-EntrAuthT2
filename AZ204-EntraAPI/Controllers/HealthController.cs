83af799f7e2d883143d9e7c232c62fb5112dd8f0
jceronapexsystems <108758000+jceronapexsystems@users.noreply.github.com>
2024-09-30 05:15:23 +0000

    health controller

---
+++ AZ204-EntraAPI/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+// web api controller to check API heatlh
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+
+namespace AZ204_EntrAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("API is healthy");
+            sb.AppendLine($"Time: {DateTime.Now}");
+            return Ok(sb.ToString());
+        }
+    }
+}
--- AZ204-EntraAPI/Program.cs
+++ AZ204-EntraAPI/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// add support for web api controllers
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
